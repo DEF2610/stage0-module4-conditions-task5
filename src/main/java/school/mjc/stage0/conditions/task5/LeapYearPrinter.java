6666cca4fdf1185911b54be36b01c06dc265bc43
Temka <maksimukart@gmail.com>
2023-01-29 20:50:43 +0100

    d

--- src/main/java/school/mjc/stage0/conditions/task5/LeapYearPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task5/LeapYearPrinter.java
@@ -2,5 +2,12 @@ package school.mjc.stage0.conditions.task5;
 
 public class LeapYearPrinter {
     public void isLeapYear(int year) {
+        switch (year){
+            case 2000,2004,2008,2012,2016,2020,2024,2028,2032,2040,2044,2048,2052,2056,2060:
+                System.out.println("leap");
+                break;
+            default:
+                System.out.println("not leap");
+        }
     }
 }
